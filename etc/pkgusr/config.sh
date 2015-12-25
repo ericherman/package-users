ffa1148261c2d46b46c402178b3f12ccf4cd66e7
Brett Neumeier <brett@neumeier.us>
2015-12-24 21:02:06 -0600

    update default package user files
    The CBL/litbuild approach suggests various differences from the original
    approach, and I have different stylistic preferences from the original
    author.

---
+++ etc/pkgusr/config.sh
@@ -0,0 +1,8 @@
+# global configuration for package users helper scripts
+# this gets sourced in by all the helper scripts
+
+export PKGUSER_HOME=/usr/src
+export PKGUSR_SKEL=/etc/pkgusr/skel-package
+
+export MINUID=10000
+export MAXUID=20000
