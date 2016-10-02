1d5eec16ff5bb595978476749e5115a6b7c5620e
Brett Neumeier <brett@neumeier.us>
2016-10-02 16:54:56 -0500

    tweaks and bugfixes

--- etc/pkgusr/config.sh
+++ etc/pkgusr/config.sh
@@ -1,7 +1,7 @@
 # global configuration for package users helper scripts
 # this gets sourced in by all the helper scripts
 
-export PKGUSER_HOME=/usr/src
+export PKGUSR_HOME=/usr/src
 export PKGUSR_SKEL=/etc/pkgusr/skel-package
 
 export MINUID=10000
